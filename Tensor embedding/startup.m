2c0ab0808ce48b6179cff01d3c45ffc0ed05ff78
ycachy <1021487797@qq.com>
2019-05-09 16:59:40 +0800

    Create startup.m

---
+++ Tensor embedding/startup.m
@@ -0,0 +1,13 @@
+run('C:\Users\YJ\Desktop\tensorsvd\pkg\vlfeat-0.9.20\toolbox\vl_setup.m');
+addpath('C:\Users\YJ\Desktop\tensorsvd\pkg\')
+addpath('C:\Users\YJ\Desktop\tensorsvd\pkg\tensor_toolbox_2.6');
+addpath('C:\Users\YJ\Desktop\tensorsvd\pkg\tensor_toolbox_2.6\met');
+addpath('C:\Users\YJ\Desktop\tensorsvd\pkg\PROPACK')
+
+
+%% LIBRARIES
+addpath('C:\Users\YJ\Desktop\tensorsvd\libs\poblano_toolbox_1.1\');
+addpath('C:\Users\YJ\Desktop\tensorsvd\libs\tensor_toolbox_2.5\');
+addpath('C:\Users\YJ\Desktop\tensorsvd\libs\nway331\');
+%addpath(genpath('src'));
+addpath(genpath('function'))
